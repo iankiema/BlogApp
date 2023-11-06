52db45b2c786bc52cc2414697108c327b2a75091
iankiema <kiemaian.ik@gmail.com>
2023-11-06 13:43:18 +0300

    Configured routes, controllers, and views for All Posts by a Given User and Single Post pages.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+  def index
+    # logic to fetch all posts by a given user goes here
+  end
+
+  def show
+    # logic to fetch a single post goes here
+  end
+end
